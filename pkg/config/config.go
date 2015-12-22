fe77d8eac2bc9a36b5fd6ae3b65a7bd398f42d85
Victor Palade <palade.ionut@gmail.com>
2015-12-22 19:53:34 +0100

    fixed a typo in a comment

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	_ "github.com/op/go-logging" // leave the logging package
-	_ "gopkg.in/yaml.v2"         // and the yaml package so that i don't forge where they are
+	_ "gopkg.in/yaml.v2"         // and the yaml package so that i don't forget where they are
 )
 
 func loadConfig() {
