7b36309b4ed409d00dbb9c1705860fcc09b6e7e3
Victor Palade <palade.ionut@gmail.com>
2016-02-06 15:12:38 +0100

    add basic tests
    - basic tests for returned metrics
    
    Signed-off-by: pi-victor <ipalade@redhat.com>

---
+++ pkg/core/metrics_test.go
@@ -0,0 +1,14 @@
+package monito
+
+import "testing"
+
+func TestMetrics(t *testing.T) {
+	t.Fail()
+}
+
+// TODO: remember to implement go chans for metrics
+func workgroupTestHelper() {
+	go func() {
+
+	}()
+}
